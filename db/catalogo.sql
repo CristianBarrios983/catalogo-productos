bab8393316e5ee6a4be42644e5cd49046f770f66
CristianBarrios983 <barrioscristian.9908@gmail.com>
2024-06-26 01:10:48 -0300

    Tabla de imagenes agregada

--- db/catalogo.sql
+++ db/catalogo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-06-2024 a las 23:42:07
+-- Tiempo de generación: 26-06-2024 a las 06:08:35
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -33,6 +33,36 @@ CREATE TABLE `categorias` (
   `descripcion` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `descripcion`) VALUES
+(6, 'Calzados', 'Todas las medidas para hombres y mujeres'),
+(7, 'Pantalones', 'Pantalones para hombres y mujeres'),
+(8, 'Remeras', 'Para hombres y mujeres'),
+(9, 'Medias', 'Niños y damas');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagenes`
+--
+
+CREATE TABLE `imagenes` (
+  `id` int(11) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `producto` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `imagenes`
+--
+
+INSERT INTO `imagenes` (`id`, `url`, `producto`) VALUES
+(6, '../../imagenes/astronauta2.jpg', 1),
+(7, '../../imagenes/125b4749900b13cbacf81faade2a5f8f.jpg', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +79,17 @@ CREATE TABLE `productos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `nombre`, `descripcion`, `precio`, `cantidad`, `id_categoria`) VALUES
+(1, 'Jeans vaqueros', 'Para hombre', 1500, 20, 7),
+(2, 'Remera Dama', 'Color rosa', 1000, 10, 8),
+(3, 'Medias para niños', 'Varios colores', 400, 20, 9),
+(5, 'Remera Cristiano Ronaldo', 'Camiseta de futbol dorsal 7 Manchester United', 6000, 20, 8),
+(6, 'Zapatillas', 'Marca Nike color negro', 10000, 10, 6);
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -57,6 +98,13 @@ CREATE TABLE `productos` (
 --
 ALTER TABLE `categorias`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `imagenes`
+--
+ALTER TABLE `imagenes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `producto` (`producto`);
 
 --
 -- Indices de la tabla `productos`
@@ -73,17 +121,29 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `imagenes`
+--
+ALTER TABLE `imagenes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `imagenes`
+--
+ALTER TABLE `imagenes`
+  ADD CONSTRAINT `imagenes_ibfk_1` FOREIGN KEY (`producto`) REFERENCES `productos` (`id`) ON DELETE CASCADE;
 
 --
 -- Filtros para la tabla `productos`
